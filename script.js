644ec2ae7175749117c6c80c86de45adf60d8afa
Jordin Gutierrez <sjgutierrez2016@gmail.com>
2024-06-05 16:58:35 -0500

    Ejercicio 2

---
+++ script.js
@@ -0,0 +1,12 @@
+let usuario; 
+let numero;
+let suma;
+
+numero = Number (prompt(`agregar numero`));
+usuario = Number (prompt(`digite numero 10`));
+
+suma = numero + usuario
+
+console.log(`resultado imprimido de ${numero} + ${usuario} es ${suma}`);
+
+document.write(`su resultado de ${numero} + ${usuario} es ${suma}`);
